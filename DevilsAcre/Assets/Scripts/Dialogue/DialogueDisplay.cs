7831c2aff26c9d143432291720dff7b1d8e8e81e
BenBeary <120347482+BenBeary@users.noreply.github.com>
2023-12-03 23:21:39 -0800

    Dialogue SFX

--- DevilsAcre/Assets/Scripts/Dialogue/DialogueDisplay.cs
+++ DevilsAcre/Assets/Scripts/Dialogue/DialogueDisplay.cs
@@ -9,20 +9,33 @@ public class DialogueDisplay : MonoBehaviour
     public static DialogueDisplay Instance;
     
     public float delay = 0.1f;
-
+    [Header("UI Items")]
     [SerializeField] private TextMeshProUGUI DisplayText;
     [SerializeField] private TextMeshProUGUI NameText;
     [SerializeField] private GameObject characterIcon;
+    [Header("UI Container")]
     [SerializeField] private GameObject DialogueUI;
-    
+    [Header("Sound Effects")]
+    [SerializeField] private int textSFXSpacing = 1;
+    [SerializeField] private AudioSource source;
+    [SerializeField] private AudioClip textSFX;
+    [SerializeField] private AudioClip buttonSFX;
+
+
     private DialogueEntry currentEntry;
     private int currentlyReading = 0;
     private string currentText = "";
     private bool textIsRunning = false;
+    private int textSFXSpeed;
     private float speed;
 
     private void Awake()
     {
+        if(source != null)
+        {
+            source.ignoreListenerPause = true; // plays through pause
+        }
+
         // Singleton Paradox Killer
         #region SINGLETON
         if (Instance == null)
@@ -40,6 +53,12 @@ public class DialogueDisplay : MonoBehaviour
 
     public void TurnOnDialogue(DialogueEntry entry)
     {
+        if(source != null)
+        {
+            source.volume = SoundManager.Instance.GetEffectVolume() - 0.4f;
+            source.mute = SoundManager.Instance.CheckForMute();
+        }
+
         DialogueUI.gameObject.SetActive(true);
         currentEntry = entry;
         currentlyReading = 0;
@@ -56,9 +75,19 @@ public class DialogueDisplay : MonoBehaviour
     IEnumerator ShowText()
     {
         textIsRunning = true;
+        if(source != null)
+        {
+            textSFXSpeed = textSFXSpacing;
+        }
         speed = delay;
         for(int i = 0; i < currentEntry.entries[currentlyReading].description.Length+1; i++)
         {
+
+            if( i % textSFXSpeed == 0 && source != null)
+            {
+                source.PlayOneShot(textSFX);
+            }
+
             currentText = currentEntry.entries[currentlyReading].description.Substring(0, i);
             DisplayText.text = currentText;
             yield return new WaitForSeconds(speed);
@@ -69,9 +98,15 @@ public class DialogueDisplay : MonoBehaviour
 
     public void playNext()
     {
+        if(source != null)
+        {
+            source.PlayOneShot(buttonSFX);
+        }
+
         if(textIsRunning)
         {
-            speed = 0.01f;
+            textSFXSpeed *= 10;
+            speed *= .1f;
             return;
         }
         else
