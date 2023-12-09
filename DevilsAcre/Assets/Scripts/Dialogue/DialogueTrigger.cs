ee5a4533bc4ff36fd3a111739c666152d230ba84
nickzorzi <nicolas.zorzi@sjsu.edu>
2023-12-09 15:50:39 -0800

    more dialogue work

---
+++ DevilsAcre/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+
+    [SerializeField] private string ThisID;
+    [SerializeField] private DialogueEntry entry;
+    [SerializeField] private GameObject[] turnOffItemsDuring;
+
+    private bool needsOpen = true;
+
+    public PlayerData playerData;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (playerData.hasKey && needsOpen)
+        {
+            DialogueTriggered();
+            needsOpen = false;
+        }
+
+
+
+        if (!DialogueDisplay.Instance.isRunning)
+        {
+            OnandOff();
+            for (int i = 0; i < PlayerData.Instance.dialogues.Length; i++)
+            {
+                if (PlayerData.Instance.dialogues[i].name == ThisID)
+                {
+                    PlayerData.Instance.dialogues[i].hasPlayed = true;
+                    break;
+                }
+
+            }
+            gameObject.SetActive(false);
+            
+        }
+    }
+
+    void DialogueTriggered()
+    {
+        for(int i = 0; i < PlayerData.Instance.dialogues.Length; i++)
+        {
+            if (PlayerData.Instance.dialogues[i].name == ThisID && PlayerData.Instance.dialogues[i].hasPlayed) {
+                gameObject.SetActive(false);
+                return;
+            }
+
+        }
+
+        
+        OnandOff();
+        DialogueDisplay.Instance.TurnOnDialogue(entry);
+    }
+
+    private void OnandOff()
+    {
+        foreach (GameObject obj in turnOffItemsDuring)
+        {
+            if(obj.GetComponent<PlayerController>())
+            {
+                obj.GetComponent<PlayerController>().enabled = !obj.GetComponent<PlayerController>().enabled;
+                continue;
+            }
+
+            obj.SetActive(!obj.activeSelf);
+
+        }
+    }
+}
