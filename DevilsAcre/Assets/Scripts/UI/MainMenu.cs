3e6928082f4f41dd83478190fc083157c8e682e2
BenBeary <120347482+BenBeary@users.noreply.github.com>
2023-12-13 14:37:34 -0800

    Death Restart Dialogue bug Fix?

--- DevilsAcre/Assets/Scripts/UI/MainMenu.cs
+++ DevilsAcre/Assets/Scripts/UI/MainMenu.cs
@@ -6,11 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
-   public Projectile projectile;
-   public PlayerController playerController;
-   public UIManager uiManager;
 
-   private Score scoreManager;
 
    public void PlayGame()
    {
@@ -23,53 +19,17 @@ public class MainMenu : MonoBehaviour
         PlayerData.Instance.ResetBools();
 
         Projectile.damage = 1;
-        playerController.damage = 1;
-        playerController.speed = 5f;
-        playerController.timeBetweenShots = 1f;
-
-        playerController.dashSpeed = 0;
-        playerController.dashCooldown = 0;
-        playerController.dashDuration = 0f;
 
         PauseMenu.GameIsPaused = false;
 
         PlayerController.allowLevelMenu = true;
         PlayerController.allowVictoryMenu = true;
 
-        playerController.canDash = false;
+        Time.timeScale = 1;
 
         AudioListener.pause = false;
-        uiManager.shoot1xUI.SetActive(true);
-        uiManager.shoot2xUI.SetActive(false);
-        uiManager.shoot125xUI.SetActive(false);
-        uiManager.dashUI.SetActive(false);
-        uiManager.axeUI.SetActive(false);
-        uiManager.molotovUI.SetActive(false);
+
    }
-    public void PlayGame2()
-    {
-        SceneManager.LoadScene(3);
-        Score.scoreValue = 0;
-
-        Projectile.damage = 1;
-        playerController.damage = 1;
-        playerController.speed = 5f;
-        playerController.timeBetweenShots = 1f;
-
-        PauseMenu.GameIsPaused = false;
-
-        PlayerController.allowLevelMenu = true;
-        PlayerController.allowVictoryMenu = true;
-
-        playerController.canDash = false;
-
-        uiManager.shoot1xUI.SetActive(true);
-        uiManager.shoot2xUI.SetActive(false);
-        uiManager.shoot125xUI.SetActive(false);
-        uiManager.dashUI.SetActive(false);
-
-        AudioListener.pause = false;
-    }
 
     public void ExitCredits()
    {
